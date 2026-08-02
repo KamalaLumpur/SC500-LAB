7fa14d56ef12e9c0da5d0803b796ec6a6da97e3c
Kamala Lumpur <kamala.lumpur7@gmail.com>
2026-08-02 13:39:02 +0100

    Week 0 Day 4 - Initial PowerShell environment setup

---
+++ PowerShell/HelloAzure.ps1
@@ -0,0 +1,15 @@
+# ==========================================
+# Hello Azure
+# Week 0 - Day 4
+# Cloud & AI Security Engineer Bootcamp
+# ==========================================
+
+Write-Host "Hello Cloud Security Engineer!"
+
+Write-Host "PowerShell Version:"
+$PSVersionTable.PSVersion
+
+Write-Host ""
+
+Write-Host "Current Azure Context:"
+Get-AzContext
